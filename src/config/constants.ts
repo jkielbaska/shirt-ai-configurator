236e499ca9abd64c15449cda2d079a05bef6aed7
Jakub <kielbaskakuba@gmail.com>
2023-06-01 14:51:56 +0200

    Add helper functions. Change to typescipt

---
+++ src/config/constants.ts
@@ -0,0 +1,39 @@
+import { tTabs, tDecalTypes } from "@/types/tConstants";
+import { swatch, fileIcon, ai, logoShirt, stylishShirt } from "../assets";
+
+export const EditorTabs: tTabs[] = [
+  {
+    name: "colorpicker",
+    icon: swatch,
+  },
+  {
+    name: "filepicker",
+    icon: fileIcon,
+  },
+  {
+    name: "aipicker",
+    icon: ai,
+  },
+];
+
+export const FilterTabs: tTabs[] = [
+  {
+    name: "logoShirt",
+    icon: logoShirt,
+  },
+  {
+    name: "stylishShirt",
+    icon: stylishShirt,
+  },
+];
+
+export const DecalTypes: tDecalTypes = {
+  logo: {
+    stateProperty: "logoDecal",
+    filterTab: "logoShirt",
+  },
+  full: {
+    stateProperty: "fullDecal",
+    filterTab: "stylishShirt",
+  },
+};
