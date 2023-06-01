7ad1b328ffd8d145a24ee63172d0d06a0648bfce
Jakub <kielbaskakuba@gmail.com>
2023-06-01 16:14:53 +0200

    Add typescript types for tabs with images

---
+++ src/types/tTabs.ts
@@ -0,0 +1,11 @@
+export interface tFilterTab {
+  logoShirt: boolean;
+  stylishShirt: boolean;
+}
+
+export const initialFilterTab: tFilterTab[] = [
+  {
+    logoShirt: true,
+    stylishShirt: false,
+  },
+];
