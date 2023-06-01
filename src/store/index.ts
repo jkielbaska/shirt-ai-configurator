5efe1fdeabacace28c945433a1ffc19635fb6d7a
Jakub <kielbaskakuba@gmail.com>
2023-06-01 14:52:25 +0200

    Add store from valtio to handle state

---
+++ src/store/index.ts
@@ -0,0 +1,19 @@
+import { proxy } from "valtio";
+
+export interface tState {
+  intro: boolean;
+  color: string;
+  isLogoTexture: boolean;
+  isFullTexture: boolean;
+  logoDecal: string;
+  fullDecal: string;
+}
+
+export const state: tState = proxy({
+  intro: true,
+  color: "#EFBD48",
+  isLogoTexture: true,
+  isFullTexture: false,
+  logoDecal: "./threejs.png",
+  fullDecal: "./threejs.png",
+});
