7d4425179b925b4c93c0f70b8a5b39b672d63421
Jakub <kielbaskakuba@gmail.com>
2023-06-02 19:34:28 +0200

    Add component with prompt and submitting buttons

--- src/components/AIPicker.tsx
+++ src/components/AIPicker.tsx
@@ -1,3 +1,49 @@
-export const AIPicker = () => {
-  return <div>AIPicker</div>;
+import { CustomButton } from "./CustomButton";
+
+export const AIPicker = ({
+  prompt,
+  setPrompt,
+  generatingImg,
+  handleSubmit,
+}: {
+  prompt: string;
+  setPrompt: (value: string) => void;
+  generatingImg: boolean;
+  handleSubmit: (event: string) => void;
+}) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="Asking AI..."
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit("logo")}
+              customStyles="text-xs"
+            />
+            <CustomButton
+              type="outline"
+              title="AI Full"
+              handleClick={() => handleSubmit("full")}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
